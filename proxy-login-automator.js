3fd1753b928df5a08daad4e87cb9af10822dead6
Dima <yudind@gmail.com>
2019-10-21 13:17:55 +0300

    disable password print out

--- proxy-login-automator.js
+++ proxy-login-automator.js
@@ -35,8 +35,9 @@ function main() {
   if (cfg.as_pac_server && (cfg.local_host === '*' || cfg.local_host === '0.0.0.0' || cfg.local_host === '::')) {
     return console.error('when use as a PAC server, the local_host parameter must be a definite address');
   }
+  var pwd=cfg.pwd; cfg.pwd='***';
   console.log('Using parameters: ' + JSON.stringify(cfg, null, '  '));
-  cfg.buf_proxy_basic_auth = new Buffer('Proxy-Authorization: Basic ' + new Buffer(cfg.usr + ':' + cfg.pwd).toString('base64'));
+  cfg.buf_proxy_basic_auth = new Buffer('Proxy-Authorization: Basic ' + new Buffer(cfg.usr + ':' + pwd).toString('base64'));
 
   if (cfg.as_pac_server) {
     createPacServer(cfg.local_host, cfg.local_port, cfg.remote_host, cfg.remote_port, cfg.buf_proxy_basic_auth, cfg.is_remote_https, cfg.ignore_https_cert, cfg.are_remotes_in_pac_https);
